8f3dc29e725cd413ac6c329eab01119f97033e63
roland-kister <roland.kister@pm.me>
2024-12-11 11:12:22 +0100

    Day 11 Part 1 improvement

--- day11/day11.go
+++ day11/day11.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 type Day11 struct {
@@ -14,7 +15,7 @@ type Day11 struct {
 }
 
 type stone struct {
-	val       int
+	val       uint64
 	nextStone *stone
 }
 
@@ -33,7 +34,7 @@ func (d *Day11) LoadInput(input io.Reader) {
 			panic(err)
 		}
 
-		currStone := &stone{val: val}
+		currStone := &stone{val: uint64(val)}
 
 		if prevStone == nil {
 			d.initStone = currStone
@@ -51,14 +52,41 @@ func (d *Day11) PartOne() int {
 }
 
 func (d *Day11) PartTwo() int {
-	return d.solve(75)
+	return d.solve(25)
 }
 
 func (d *Day11) solve(iterCount int) int {
 	initStone := d.copyInitStone()
 
+	initStoneCount := 0
+	for s := initStone; s != nil; s = s.nextStone {
+		initStoneCount++
+	}
+
+	sumChan := make(chan int, initStoneCount)
+	wg := new(sync.WaitGroup)
+
+	for s := initStone; s != nil; s = s.nextStone {
+		wg.Add(1)
+		go s.solveRoutine(iterCount, sumChan, wg)
+	}
+
+	wg.Wait()
+	close(sumChan)
+
+	sum := 0
+	for subSum := range sumChan {
+		sum += subSum
+	}
+
+	return sum
+}
+
+func (s *stone) solveRoutine(iterCount int, sumChan chan<- int, wg *sync.WaitGroup) {
+	s.nextStone = nil
+
 	for range iterCount {
-		curr := initStone
+		curr := s
 
 		for curr != nil {
 			switch {
@@ -75,11 +103,12 @@ func (d *Day11) solve(iterCount int) int {
 	}
 
 	sum := 0
-	for s := initStone; s != nil; s = s.nextStone {
+	for curr := s; curr != nil; curr = curr.nextStone {
 		sum++
 	}
 
-	return sum
+	sumChan <- sum
+	wg.Done()
 }
 
 func (d *Day11) copyInitStone() *stone {
@@ -105,40 +134,69 @@ func (d *Day11) copyInitStone() *stone {
 	return initStone
 }
 
-func (s *stone) countDigits() int {
-	tmp := s.val
-	count := 0
+func (s *stone) divider() uint64 {
+	n := s.val
 
-	for tmp != 0 {
-		tmp /= 10
-		count++
+	switch {
+	case n == 0:
+		return 0
+	case n < 10:
+		return 0
+	case n < 100:
+		return 10
+	case n < 1_000:
+		return 0
+	case n < 10_000:
+		return 100
+	case n < 100_000:
+		return 0
+	case n < 1_000_000:
+		return 1_000
+	case n < 10_000_000:
+		return 0
+	case n < 100_000_000:
+		return 10_000
+	case n < 1_000_000_000:
+		return 0
+	case n < 10_000_000_000:
+		return 100_000
+	case n < 100_000_000_000:
+		return 0
+	case n < 1_000_000_000_000:
+		return 1_000_000
+	case n < 10_000_000_000_000:
+		return 0
+	case n < 100_000_000_000_000:
+		return 10_000_000
+	case n < 1_000_000_000_000_000:
+		return 0
+	case n < 10_000_000_000_000_000:
+		return 100_000_000
+	case n < 100_000_000_000_000_000:
+		return 0
+	case n < 1_000_000_000_000_000_000:
+		return 1_000_000_000
+	default:
+		return 100_000
 	}
-
-	return count
 }
 
 func (s *stone) split() bool {
-	digitCount := s.countDigits()
+	divider := s.divider()
 
-	if digitCount%2 != 0 {
+	if divider == 0 {
 		return false
 	}
 
+	newVal := s.val % divider
+	s.val = s.val / divider
+
 	newStone := &stone{
-		val:       0,
+		val:       newVal,
 		nextStone: s.nextStone,
 	}
 
 	s.nextStone = newStone
-
-	multiplier := 1
-	for range digitCount / 2 {
-		part := (s.val % 10) * multiplier
-		newStone.val += part
-		multiplier *= 10
-
-		s.val /= 10
-	}
 
 	return true
 }
