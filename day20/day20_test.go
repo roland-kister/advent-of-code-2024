41d76b0a45c66dbce75a13750e6f067a7bb870ae
roland-kister <roland.kister@pm.me>
2024-12-25 09:56:50 +0100

    Day 20 init

---
+++ day20/day20_test.go
@@ -0,0 +1,47 @@
+package day20
+
+import (
+	"strings"
+	"testing"
+)
+
+const example = `###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############`
+
+func TestPartOne(t *testing.T) {
+	d := Day20{}
+	d.LoadInput(strings.NewReader(example))
+	d.savedPicoS = 20
+
+	out := 5
+	actOut := d.PartOne()
+
+	if out != actOut {
+		t.Fatalf("PartOne() = %d, want %d", actOut, out)
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	d := Day20{}
+	d.LoadInput(strings.NewReader(example))
+
+	out := 0
+	actOut := d.PartTwo()
+
+	if out != actOut {
+		t.Fatalf("PartTwo() = %d, want %d", actOut, out)
+	}
+}
