a49932a81d75ffd74204f85e17e1129388671569
roland-kister <roland.kister@pm.me>
2025-01-03 12:51:17 +0100

    Day 24 Part 2

--- day24/day24.go
+++ day24/day24.go
@@ -8,7 +8,8 @@ import (
 	"io"
 	"math"
 	"regexp"
-	"strconv"
+	"sort"
+	"strings"
 )
 
 type Day24 struct {
@@ -16,10 +17,10 @@ type Day24 struct {
 	gates map[string]logicGate
 }
 
-type logcOp uint8
+type logicOp uint8
 
 const (
-	andOp logcOp = iota
+	andOp logicOp = iota
 	orOp
 	xorOp
 )
@@ -31,7 +32,7 @@ type logicGate struct {
 	wireA   string
 	wireB   string
 	wireOut string
-	op      logcOp
+	op      logicOp
 }
 
 func (lg logicGate) combine() {
@@ -98,7 +99,7 @@ func (d *Day24) LoadInput(input io.Reader) {
 
 		opStr := match[2]
 
-		var op logcOp
+		var op logicOp
 		switch opStr {
 		case "AND":
 			op = andOp
@@ -119,74 +120,177 @@ func (d *Day24) LoadInput(input io.Reader) {
 }
 
 func (d *Day24) PartOne() int {
-	res := 0
-
-	for wireOut := range d.gates {
-		if wireOut[0] != 'z' {
-			continue
-		}
-
-		outIndex, err := strconv.Atoi(wireOut[1:])
-		if err != nil {
-			panic(err)
-		}
-
-		d.execute(wireOut)
-
-		res |= (int(d.wires[wireOut]) << outIndex)
+	outputWires := d.getOutputWires()
+	for _, wire := range outputWires {
+		d.execute(wire)
 	}
 
-	return res
+	return d.getNumber('z')
 }
 
 func (d *Day24) PartTwo() int {
-	xVal := 0
-	yVal := 0
-	zReal := 0
+	invalidGates := d.getInvalidWires()
+	sort.Strings(invalidGates)
 
-	for wire, val := range d.wires {
-		if wire[0] != 'x' && wire[0] != 'y' && wire[0] != 'z' {
+	res := strings.Join(invalidGates, ",")
+	fmt.Printf("\tpart two = %s\n", res)
+
+	return 0
+}
+
+func (d *Day24) getOutputWires() map[int]string {
+	gates := make(map[int]string)
+
+	for wire := range d.wires {
+		if wire[0] != 'z' {
 			continue
 		}
 
-		zIndex, err := strconv.Atoi(wire[1:])
-		if err != nil {
-			panic(err)
-		}
+		bitIndex := int(wire[1]-'0')*10 + int(wire[2]-'0')
 
-		shiftedVal := (int(val) << zIndex)
-
-		if wire[0] == 'x' {
-			xVal |= shiftedVal
-		} else if wire[0] == 'y' {
-			yVal |= shiftedVal
-		} else {
-			zReal |= shiftedVal
-		}
+		gates[bitIndex] = wire
 	}
 
-	zExpected := xVal + yVal
-
-	fmt.Println(xVal)
-	fmt.Println(yVal)
-	fmt.Println(zReal)
-	fmt.Println(zExpected)
-
-	return 1
+	return gates
 }
 
 func (d *Day24) execute(wireOut string) {
 	gate := d.gates[wireOut]
 
-	wireAVal := d.wires[gate.wireA]
-	if wireAVal == undefined {
+	if gate.wireA[0] != 'x' && gate.wireA[0] != 'y' {
 		d.execute(gate.wireA)
 	}
 
-	wireBVal := d.wires[gate.wireB]
-	if wireBVal == undefined {
+	if gate.wireB[0] != 'x' && gate.wireB[0] != 'y' {
 		d.execute(gate.wireB)
 	}
 
 	gate.combine()
+}
+
+func (d *Day24) getNumber(char byte) int {
+	num := 0
+
+	for wire, val := range d.wires {
+		if wire[0] != char {
+			continue
+		}
+
+		bitIndex := int(wire[1]-'0')*10 + int(wire[2]-'0')
+
+		num |= int(val) << bitIndex
+	}
+
+	return num
+}
+
+func (d *Day24) getInvalidWires() []string {
+	lastZ := len(d.getOutputWires()) - 1
+
+	invalidWires := make([]string, 0)
+
+	for _, gate := range d.gates {
+		invalid, isValid := d.isValidOutput(gate, lastZ)
+		if !isValid {
+			invalidWires = append(invalidWires, invalid)
+			continue
+		}
+
+		invalid, isValid = d.isValidAnd(gate)
+		if !isValid {
+			invalidWires = append(invalidWires, invalid)
+			continue
+		}
+
+		invalid, isValid = d.isValidXor(gate)
+		if !isValid {
+			invalidWires = append(invalidWires, invalid)
+			continue
+		}
+
+		invalid, isValid = d.isValidOr(gate)
+		if !isValid {
+			invalidWires = append(invalidWires, invalid)
+			continue
+		}
+	}
+
+	return invalidWires
+}
+
+func (d *Day24) isValidOutput(gate logicGate, lastZ int) (string, bool) {
+	if gate.wireOut[0] != 'z' {
+		return "", true
+	}
+
+	zIndex := int(gate.wireOut[1]-'0')*10 + int(gate.wireOut[2]-'0')
+	if zIndex != lastZ && gate.op != xorOp {
+		return gate.wireOut, false
+	}
+
+	return "", true
+}
+
+func (d *Day24) isValidAnd(gate logicGate) (string, bool) {
+	if gate.op != andOp {
+		return "", true
+	}
+
+	prevGate, ok := d.gates[gate.wireA]
+	if ok && prevGate.op == andOp {
+		xIndexA := int(prevGate.wireA[1]-'0')*10 + int(prevGate.wireA[2]-'0')
+		xIndexB := int(prevGate.wireB[1]-'0')*10 + int(prevGate.wireB[2]-'0')
+
+		if xIndexA != 0 && xIndexB != 0 {
+			return gate.wireA, false
+		}
+	}
+
+	prevGate, ok = d.gates[gate.wireB]
+	if ok && prevGate.op == andOp {
+		xIndexA := int(prevGate.wireA[1]-'0')*10 + int(prevGate.wireA[2]-'0')
+		xIndexB := int(prevGate.wireB[1]-'0')*10 + int(prevGate.wireB[2]-'0')
+
+		if xIndexA != 0 && xIndexB != 0 {
+			return gate.wireB, false
+		}
+	}
+
+	return "", true
+}
+
+func (d *Day24) isValidOr(gate logicGate) (string, bool) {
+	if gate.op != orOp {
+		return "", true
+	}
+
+	prevGate, ok := d.gates[gate.wireA]
+	if ok && prevGate.op == xorOp {
+		_, valid := d.isValidXor(prevGate)
+		if valid {
+			return gate.wireA, false
+		}
+	}
+
+	prevGate, ok = d.gates[gate.wireB]
+	if ok && prevGate.op == xorOp {
+		_, valid := d.isValidXor(prevGate)
+		if valid {
+			return gate.wireB, false
+		}
+	}
+
+	return "", true
+}
+
+func (d *Day24) isValidXor(gate logicGate) (string, bool) {
+	if gate.op != xorOp {
+		return "", true
+	}
+
+	if gate.wireA[0] != 'x' && gate.wireA[0] != 'y' && gate.wireB[0] != 'x' && gate.wireB[0] != 'y' && gate.wireOut[0] != 'z' {
+		return gate.wireOut, false
+	}
+
+	return "", true
 }
